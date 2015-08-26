467138b901f431fb62732f7522d89ef94e670a49
qioixiy <qioixiy@gmail.com>
2015-08-26 23:23:01 +0800

    android-mirror.mk

---
+++ android/android-mirror.mk
@@ -0,0 +1,24 @@
+## google android mirror
+`
+mkdir /usr/local/android-mirror
+cd /usr/local/android-mirror
+repo init --mirror -u https://android.googlesource.com/mirror/manifest
+repo sync
+mkdir /usr/local/android-2.3.7
+cd /usr/local/android-2.3.7
+repo init -u /usr/local/mirror/platform/manifest.git -b android-2.3.7_r1
+repo sync
+repo init -u https://android.googlesource.com/platform/manifest
+repo sync
+`
+
+##
+curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+mkdir WORKING_DIRECTORY
+cd WORKING_DIRECTORY
+repo init -u git://git.omapzoom.org/platform/manifest
+repo init -u git://git.omapzoom.org/platform/manifest -b android-2.3.5_r1
+repo sync
+
+## Android Dev Tools[url](http://www.androiddevtools.cn)
